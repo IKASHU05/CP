3fe06645380edbe93c550dc3e07dc5ee4b8ea243
IRISH <virus200313@gmail.com>
2023-12-30 00:02:47 +0530

    initial commit

---
+++ Practice/1915A.py
@@ -0,0 +1,15 @@
+import math
+
+def solve():
+    # If n and then array is given
+    a,b,c = [int(x) for x in input().split()]
+    print(a^b^c)
+
+
+
+t = 1
+t = int(input())
+
+while t > 0:
+    solve()
+    t -= 1
